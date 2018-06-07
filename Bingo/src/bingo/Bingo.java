1fdfb2820126d126cee90782df6a5d152f1540e0
Ankit Pasayat <ankitpasayat@gmail.com>
2018-06-07 11:17:18 +0530

    Update Bingo.java
    user can set board size and can choose between entering the numbers or using a random board

--- Bingo/src/bingo/Bingo.java
+++ Bingo/src/bingo/Bingo.java
@@ -12,23 +12,40 @@ import java.util.Scanner;
  */
 public class Bingo {
 
+    static int n, n2;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner sc = new Scanner(System.in);
-        int[] NUM1 = new int[25];
-        int[] NUM2 = new int[25];
-        int[] used = new int[25];
-        for(int i = 0; i <25;){
-            int temp = (int)(Math.random()*25) + 1;
-            if(!containsVal(NUM1, temp)) {
-                NUM1[i++] = temp;
+        startGame();
+        int[] NUM1 = new int[n2];
+        int[] NUM2 = new int[n2];
+        int[] used = new int[n2];
+        System.out.println("Do You Want To Fill The Board Yourlself (Y/N)? ");
+        char choice = sc.nextLine().charAt(0);
+        switch(choice){
+            case 'Y':
+                System.out.println("ENTER THE NUMBERS: ");
+                for(int i = 0; i <n2;){
+                    int temp = Integer.parseInt(sc.nextLine());
+                    if(!containsVal(NUM1, temp)) {
+                        NUM1[i++] = temp;
+                }
             }
+                break;
+            case 'N':
+                for(int i = 0; i <n2;){
+                    int temp = (int)(Math.random()*n2) + 1;
+                    if(!containsVal(NUM1, temp)) {
+                        NUM1[i++] = temp;
+                }
+            }
+                break;
         }
-        for(int i = 0; i <25;){
-            int temp = (int)(Math.random()*25) + 1;
+        for(int i = 0; i <n2;){
+            int temp = (int)(Math.random()*n2) + 1;
             if(!containsVal(NUM2, temp)) {
                 NUM2[i++] = temp;
             }
@@ -40,24 +57,24 @@ public class Bingo {
         int u = 0, row_status1, col_status1, row_status2, col_status2, num, index1, index2, curr_row, curr_column;
         int bingo1 = 0, bingo2 = 0;
         int[] row1;
-        row1 = new int[5];
+        row1 = new int[n];
         int[] column1;
-        column1 = new int[5];
+        column1 = new int[n];
         int[] row2;
-        row2 = new int[5];
+        row2 = new int[n];
         int[] column2;
-        column2 = new int[5];
+        column2 = new int[n];
         int[] diag1;
-        diag1 = new int[2];
+        diag1 = new int[n];
         int[] diag2;
-        diag2 = new int[2];
+        diag2 = new int[n];
         initialize_diagonal(diag1);
         initialize_diagonal(diag2);
-        for(int t = 0; t < 25;){
+        for(int t = 0; t < n2;){
             if(t % 2 == 0){
                 System.out.println("Enter your number: ");
                 num = sc.nextInt();
-                if(num > 0 && num < 26 && !containsVal(used, num)){
+                if(num > 0 && num < n2 + 1 && !containsVal(used, num)){
                     used[u++] = num;
                     t++;
                 }
@@ -69,21 +86,21 @@ public class Bingo {
                 index2 = findIndex(NUM2, num);
                 NUM2[index2] = 0;
                 System.out.println("INDEX 2: " + index2);
-                curr_column = index1 % 5;
+                curr_column = index1 % n;
                 curr_row = 0;
-                while(index1 > 4){
+                while(index1 > n - 1){
                     curr_row++;
-                    index1 -= 5;
+                    index1 -= n;
                 }
                 System.out.println("CURRENT ROW 1: " + curr_row);
                 System.out.println("CURRENT COLUMN 1: " + curr_column);
                 row1[curr_row]++;
                 column1[curr_column]++;
-                curr_column = index2 % 5;
+                curr_column = index2 % n;
                 curr_row = 0;
-                while(index2 > 4){
+                while(index2 > n - 1){
                     curr_row++;
-                    index2 -= 5;
+                    index2 -= n;
                 }
                 System.out.println("CURRENT ROW 2: " + curr_row);
                 System.out.println("CURRENT COLUMN 2: " + curr_column);
@@ -97,19 +114,19 @@ public class Bingo {
                 col_status1 = check_col(column1);
                 row_status2 = check_row(row2);
                 col_status2 = check_col(column2);
-                if(row_status1 < 6){
+                if(row_status1 < n + 1){
                     bingo1++;
                     row1[row_status1] = 0;
                 }
-                if(col_status1 < 6){
+                if(col_status1 < n + 1){
                     bingo1++;
                     column1[col_status1] = 0;
                 }
-                if(row_status2 < 6){
+                if(row_status2 < n + 1){
                     bingo2++;
                     row2[row_status2] = 0;
                 }
-                if(col_status2 < 6){
+                if(col_status2 < n + 1){
                     bingo2++;
                     column2[col_status2] = 0;
                 }
@@ -145,7 +162,7 @@ public class Bingo {
                 }
             }
             else{
-                num = (int)(Math.random()*25) + 1;
+                num = (int)(Math.random()*n2) + 1;
                 if(!containsVal(used, num)){
                     used[u++] = num;
                     t++;
@@ -159,21 +176,21 @@ public class Bingo {
                 index2 = findIndex(NUM2, num);
                 NUM2[index2] = 0;
                 System.out.println("INDEX 2: " + index2);
-                curr_column = index1 % 5;
+                curr_column = index1 % n;
                 curr_row = 0;
-                while(index1 > 4){
+                while(index1 > n - 1){
                     curr_row++;
-                    index1 -= 5;
+                    index1 -= n;
                 }
                 System.out.println("CURRENT ROW 1: " + curr_row);
                 System.out.println("CURRENT COLUMN 1: " + curr_column);
                 row1[curr_row]++;
                 column1[curr_column]++;
-                curr_column = index2 % 5;
+                curr_column = index2 % n;
                 curr_row = 0;
-                while(index2 > 4){
+                while(index2 > n - 1){
                     curr_row++;
-                    index2 -= 5;
+                    index2 -= n;
                 }
                 System.out.println("CURRENT ROW 2: " + curr_row);
                 System.out.println("CURRENT COLUMN 2: " + curr_column);
@@ -187,23 +204,22 @@ public class Bingo {
                 col_status1 = check_col(column1);
                 row_status2 = check_row(row2);
                 col_status2 = check_col(column2);
-                if(row_status1 < 6){
+                if(row_status1 < n + 1){
                     bingo1++;
                     row1[row_status1] = 0;
                 }
-                if(col_status1 < 6){
+                if(col_status1 < n + 1){
                     bingo1++;
                     column1[col_status1] = 0;
                 }
-                if(row_status2 < 6){
+                if(row_status2 < n + 1){
                     bingo2++;
                     row2[row_status2] = 0;
                 }
-                if(col_status2 < 6){
+                if(col_status2 < n + 1){
                     bingo2++;
                     column2[col_status2] = 0;
                 }
-                
                 if(diag1[0] == 0 || diag1[1] == 0){
                     int lf = diag1[0] == 1 ? 0 : checkLeftDiag(NUM1);
                     int rt = diag1[1] == 1 ? 0 : checkRightDiag(NUM1);
@@ -239,7 +255,7 @@ public class Bingo {
     }
     public static int findIndex(int[] item_set, int temp){
         int i;
-        for(i = 0; i < 25; i++){
+        for(i = 0; i < n2; i++){
             if(item_set[i] == temp)
                 break;
         }
@@ -256,23 +272,23 @@ public class Bingo {
     }
 
     public static int check_row(int[] row) {
-        for(int i = 0; i < 5; i++){
-            if(row[i] == 5)
+        for(int i = 0; i < n; i++){
+            if(row[i] == n)
                 return i;
         }
-        return 6;
+        return n + 1;
     }
 
     public static int check_col(int[] column) {
-        for(int i = 0; i < 5; i++){
-            if(column[i] == 5)
+        for(int i = 0; i < n; i++){
+            if(column[i] == n)
                 return i;
         }
-        return 6;
+        return n + 1;
     }
 
     private static int checkLeftDiag(int[] NUM) {
-        for(int i = 0; i < 25; i += 6){
+        for(int i = 0; i < n2; i += (n + 1)){
             if(NUM[i] != 0)
                 return 0;
         }
@@ -280,7 +296,7 @@ public class Bingo {
     }
 
     private static int checkRightDiag(int[] NUM) {
-        for(int i = 4; i < 25; i += 4){
+        for(int i = n - 1; i < n2; i += (n - 1)){
             if(NUM[i] != 0)
                 return 0;
         }
@@ -294,8 +310,8 @@ public class Bingo {
 
     private static void print_matrix(int[] NUM) {
         int c = 0;
-        for(int i = 0; i < 5; i++){
-            for(int j = 0; j < 5; j++){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
                 if(NUM[c] == 0){
                     System.out.print("X ");
                     c++;
@@ -309,7 +325,7 @@ public class Bingo {
     }
 
     private static boolean check_bingo(int t, int bingo1, int bingo2) {
-        if(bingo1 == 5){
+        if(bingo1 == n){
             if(t%2 == 0){
                 System.out.println("COMPUTER WINS!!!");
                 return true;
@@ -317,7 +333,7 @@ public class Bingo {
             System.out.println("USER WINS!!!");
             return true;
         }
-        if(bingo2 == 5){
+        if(bingo2 == n){
             if(t % 2 == 0){
                 System.out.println("USER WINS!!!");
                 return true;
@@ -326,5 +342,12 @@ public class Bingo {
             return true;
         }
         return false;
+    }
+
+    private static void startGame() {
+        System.out.println("ENTER BOARD LENGTH: ");
+        Scanner sc = new Scanner(System.in);
+        n = Integer.parseInt(sc.nextLine());
+        n2 = n * n;
     }
 }
